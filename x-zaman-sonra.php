bac25bb50cd74d21a1654a7f7c45318c82191730
Can Berk KARS <56763919+canberkkars@users.noreply.github.com>
2021-11-29 22:56:12 +0300

    Update x-zaman-sonra.php

--- x-zaman-sonra.php
+++ x-zaman-sonra.php
@@ -5,3 +5,19 @@ echo date("Y-m-d", strtotime(date("Y-m-d") . "+1 days"));   //GUN
 echo date("Y-m-d", strtotime(date("Y-m-d") . "+1 weeks"));  //HAFTA
 echo date("Y-m-d", strtotime(date("Y-m-d") . "+1 month"));  //AY
 echo date("Y-m-d", strtotime(date("Y-m-d") . "+1 year"));   //YIL
+
+//*****ORNEK * BASLANGICI*****//
+/* 1 YIL SONRASI
+$simdiki_tarih = date("Ymd");
+$yeni_tarih = date("Y.m.d", strtotime($simdiki_tarih . '+1 year')); 
+
+echo $yeni_tarih;
+*/
+
+/* 1 YIL ONCE
+$simdiki_tarih = date("Ymd");
+$yeni_tarih = date("Y.m.d", strtotime($simdiki_tarih . '-1 years')); 
+
+echo $yeni_tarih;
+*/
+//*****ORNEK * BITISI*****//
