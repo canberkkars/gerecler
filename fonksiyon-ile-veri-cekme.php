68b74f49dfa9c31fb1eae93bfe638c5d608bd7a7
Can Berk KARS <56763919+canberkkars@users.noreply.github.com>
2021-11-29 23:08:07 +0300

    Update fonksiyon-ile-veri-cekme.php

--- fonksiyon-ile-veri-cekme.php
+++ fonksiyon-ile-veri-cekme.php
@@ -1,11 +1,11 @@
  <?php
   function kategoriGetir($kategori_id, $neistedi){ global $db;
-		$kategorisor	=  $db->prepare("SELECT * FROM kategoriler WHERE id = :id");
-		$kategorisor	-> execute(array("id"=> $kategori_id));
-		$kategori		=  $kategorisor->fetch(PDO::FETCH_ASSOC);
-		
-		if ($neistedi == "kategori_adi"){ return $kategori["ad"]; }
-		elseif($neistedi == "kategori_seo"){ return $kategori["kategori_seo"]; }
+	$kategorisor	=  $db->prepare("SELECT * FROM kategoriler WHERE id = :id");
+	$kategorisor	-> execute(array("id"=> $kategori_id));
+	$kategori		=  $kategorisor->fetch(PDO::FETCH_ASSOC);
+
+	if ($neistedi == "kategori_adi"){ return $kategori["ad"]; }
+	elseif($neistedi == "kategori_seo"){ return $kategori["kategori_seo"]; }
 	}
 //*****ORNEK * BASLANGICI*****//
 echo kategoriGetir($veri["id"],"kategori_adi");
